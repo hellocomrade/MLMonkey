94c7f0c4eda2367cb80c5fdd1948171e25b9602f
hellocomrade <sun8726@hotmail.com>
2017-02-13 14:50:52 -0500

    Add LogisticRegression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from iris import iris_monkey
+from boston import boston_monkey
+from breastca import breast_monkey
 import numpy as np
 
 if "__main__" == __name__:
@@ -8,3 +10,14 @@ if "__main__" == __name__:
     iris.test()
     #passing 1d ndarray is deprecated
     print(iris.predict(np.array([[1,2,3,4],[5, 2.9, 1, 0.2]])))
+    boston = boston_monkey()
+    boston.linearRegression()
+    boston.ridgeRegression()
+    #
+    breast = breast_monkey()
+    breast.summary()
+    breast.trainAndScore()
+    breast.trainAndScore(100)
+    #testing score will be higher than training set if C is very small
+    breast.trainAndScore(0.001)
+
